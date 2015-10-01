1e47841d4cf32bb538bef16d9b9972e90ce83662
Félix Pelletier <felixpelletier23@gmail.com>
2015-10-01 16:47:49 -0400

    Initial commit

---
+++ UltimateStrategy.py
@@ -0,0 +1,15 @@
+from RULEngine.Strategy.Strategy import Strategy
+
+class ExampleStrategy(Strategy):
+    def __init__(self, field, referee, team, opponent_team, is_team_yellow=False):
+        Strategy.__init__(self, field, referee, team, opponent_team, is_team_yellow)
+
+    def on_start(self):
+        pass
+        #Code here
+
+    def on_halt(self):
+        self.on_start() #No referee yet.
+
+    def on_stop(self):
+        self.on_start() #No referee yet.
---
+++ main.py
@@ -0,0 +1,6 @@
+from RULEngine.Framework import start_game
+from ExampleStrategy import UltimateStrategy
+
+if __name__ == '__main__':
+    start_game(ExampleStrategy)
+
